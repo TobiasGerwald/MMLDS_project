35bcf28e309567b38c9d32f82cdbf5392be9b29d
tobiasgerwald <71821271+TobiasGerwald@users.noreply.github.com>
2023-02-20 12:32:18 +0100

    update packages

--- src/MMLDS_project.jl
+++ src/MMLDS_project.jl
@@ -1,6 +1,6 @@
 module MMLDS_project
 
 # Write your package code here.
-    using DynamicalSystems, CairoMakie, Printf, DifferentialEquations, Plots
+    using DynamicalSystems, CairoMakie, Printf, DifferentialEquations, Plots, OrdinaryDiffEq, NODEData, Printf, Flux, DiffEqSensitivity, Parameters, Hyperopt, StatsBase
 
 end
