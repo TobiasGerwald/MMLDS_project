9f9bd8b6fbcf0f01873e8b59dd523e1a10ad9690
tobiasgerwald <71821271+TobiasGerwald@users.noreply.github.com>
2023-02-20 12:45:29 +0100

    add ReservoirComputing

--- src/MMLDS_project.jl
+++ src/MMLDS_project.jl
@@ -1,6 +1,6 @@
 module MMLDS_project
 
 # Write your package code here.
-    using DynamicalSystems, CairoMakie, Printf, DifferentialEquations, Plots, OrdinaryDiffEq, NODEData, Printf, Flux, DiffEqSensitivity, Parameters, Hyperopt, StatsBase
+    using DynamicalSystems, CairoMakie, Printf, DifferentialEquations, Plots, OrdinaryDiffEq, NODEData, Printf, Flux, DiffEqSensitivity, Parameters, Hyperopt, StatsBase, ReservoirComputing
 
 end
