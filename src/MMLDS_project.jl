b180922e30dc509ce7ed257a7730e3871d39b746
tomf97 <tom_fischer1997@gmx.de>
2023-02-28 14:51:50 +0100

    A dataloader for the El Nino Data is ready. On Toms computer, no other file than a .ipynb works, so I will upload them now, we can delete it if we fixed the issue.

--- src/MMLDS_project.jl
+++ src/MMLDS_project.jl
@@ -1,12 +1,12 @@
 module MMLDS_project
 
 # Write your package code here.
-    using DynamicalSystems, CairoMakie, DifferentialEquations, Plots, OrdinaryDiffEq, NODEData, Printf, Flux, DiffEqSensitivity, Parameters, Hyperopt, StatsBase, ReservoirComputing, DataDrivenDiffEq, DataDrivenSparse
+    using DynamicalSystems, CairoMakie, DifferentialEquations, Plots, OrdinaryDiffEq, NODEData, Printf, Flux, DiffEqSensitivity, Parameters, Hyperopt, StatsBase, ReservoirComputing, DataDrivenDiffEq, DataDrivenSparse, NetCDF
     include("data.jl")
     include("SINDy.jl")
     include("NODE.jl")
     include("Reservoir_Computing.jl")
 
-    export create_data, train_SINDy, hyperOpt
+    export create_data, train_SINDy, hyperOpt, generate_esn, train_val_test_split, ESNHyperparams, cross_validate_esn
 
 end
--- src/data.jl
+++ src/data.jl
@@ -1,4 +1,7 @@
 using OrdinaryDiffEq
+#for loading El Nino Data
+using NetCDF
+
 ### Create training Data ###
 
 function lorenz(u, p, t)
@@ -23,4 +26,55 @@ function create_data(x0 = [1.,1.,1.],dt = 0.01)
 
     return sol
 
+end
+
+#Loading and changing El Nino Data
+path_to_data = "sst.mon.mean.nc"
+
+x = ncread(filename, "sst") #sst = Sea Surface Temperature
+x_reduced = x[121:170, 86:95, :] #only concerned with the important region for the El Nino happening
+
+function sum_elements_without_empty_values(y)
+    h,w = size(y)
+    counter = 0
+    summation = 0
+    for i in 1:h
+        for j in 1:w
+            if y[i,j] != 1.0f20
+                summation = summation + y[i,j]
+                counter = counter + 1
+            end
+        end
+    end
+    return summation/counter #using the mean here
+end
+
+function sum_elements_without_empty_values_vec(y) #EMPTY VS ZERO
+    bool_matrix = y .< 1.0f20 
+    nonzero_elements = sum(bool_matrix)
+    y_adjusted = y .* bool_matrix
+    summation = sum(y_adjusted)
+    return summation/nonzero_elements
+end
+
+function compress_data_matrix(x_data, kernel_size, vectorized = true)
+    h,w,d = size(x_data)
+    n_h = trunc(Int, h/kernel_size)
+    n_w = trunc(Int, w/kernel_size)
+    A = ones(n_h, n_w,d)
+    for dim in 1:d
+        for i in 1:n_h
+            filter_region_h = (1 + (i-1)*kernel_size): (i*kernel_size)
+            for j in 1:n_w
+                filter_region_w = (1 + (j-1)*kernel_size): (j*kernel_size)
+                y = x_reduced[filter_region_h, filter_region_w, dim]
+                if vectorized
+                    A[i,j,dim] = sum_elements_without_empty_values_vec(y)
+                else
+                    A[i,j,dim] = sum_elements_without_empty_values(y)
+                end
+            end
+        end
+    end
+    return A
 end
