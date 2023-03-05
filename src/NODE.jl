a87dda4d706326ee293a8da04c12f06551a53dae
tobiasgerwald <71821271+TobiasGerwald@users.noreply.github.com>
2023-03-05 18:01:25 +0100

    add custom suggestion for res

--- src/NODE.jl
+++ src/NODE.jl
@@ -27,29 +27,44 @@ function (m::ChaoticNDE)(X,p=m.p)
 end
 #############################################################
 
+null(u,t) = 0
 
-function hyperOpt(ho, ode_sol, x0, dt, N_epochs = 50)
+function hyperOpt(ho, ode_sol, x0, dt, N_epochs = 50, rhs_sug = null)#, p_rhs = nothing)
 
     train, valid = NODEDataloader(ode_sol, 100; dt=dt, valid_set=0.5)
 
     best_model = []
+    best_neural_ode = []
     best_val_loss = Inf
+
+    n_in_out = length(ode_sol.u[1])
+
 
     for (i,N_weights,N_hidden_layers,activation,τ_max,eta_decrease,reg) in ho #,eta
 
         hidden_layers = [Flux.Dense(N_weights, N_weights, activation) for i=1:N_hidden_layers]
-        nn = Chain(Flux.Dense(3, N_weights, activation), hidden_layers...,  Flux.Dense(N_weights, 3)) |> gpu
+        nn = Chain(Flux.Dense(n_in_out, N_weights, activation), hidden_layers...,  Flux.Dense(N_weights, n_in_out)) |> gpu
 
         p, re_nn = Flux.destructure(nn)
-        neural_ode(u, p, t) = re_nn(p)(u)
-        node_prob = ODEProblem(neural_ode, x0, (Float32(0.),Float32(dt)), p)
+        node(u, p, t) = re_nn(p)(u)
+
+
+        #p_n = [p_rhs, p]
+        function neural_ode(u, p, t)
+            #p_rhs, p_node = p
+            return rhs_sug(u, t) .+ node(u, p, t) #, p_rhs,
+        end
+            
+        node_prob = ODEProblem(neural_ode, x0, (Float32(0.),Float32(dt)), p)#_n)
         model = ChaoticNDE(node_prob)
 
-        loss = Flux.Losses.mse
+        #loss = Flux.Losses.mse
+        loss(x,y) = sum(abs2, x - y)
 
-        η = 1f-4#eta
+        η = 1f-4
         opt = Flux.AdamW(η)
         opt_state = Flux.setup(opt, model)
+        
 
         println("starting training with N_EPOCHS= ",N_epochs, " - N_weights=",N_weights, " - N_hidden_layers=",N_hidden_layers, " - activation=",activation, " - reg=",reg)
         for i_τ = 20:10:τ_max
@@ -83,11 +98,12 @@ function hyperOpt(ho, ode_sol, x0, dt, N_epochs = 50)
         if val_loss < best_val_loss
             println("update best_model; new best_val_loss = ", val_loss)
             best_model = model
+            best_neural_ode = neural_ode
             best_val_loss = val_loss
         end
 
     end
 
-    return best_model
+    return [best_model, best_neural_ode]
 
 end
