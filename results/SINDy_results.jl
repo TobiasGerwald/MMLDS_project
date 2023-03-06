31a3f84afc067d671812d660a149552337848482
tobiasgerwald <71821271+TobiasGerwald@users.noreply.github.com>
2023-03-06 11:52:51 +0100

    updated

--- results/SINDy_results.jl
+++ results/SINDy_results.jl
@@ -1,7 +1,7 @@
 import Pkg
 Pkg.activate("MMLDS_project")
 using MMLDS_project
-using OrdinaryDiffEq, Plots
+using OrdinaryDiffEq, Plots, DataDrivenDiffEq
 
 ode_sol = create_data()
 ddsol = train_SINDy(ode_sol, 4, 1e-1, 0)
@@ -9,13 +9,3 @@ ddsol = train_SINDy(ode_sol, 4, 1e-1, 0)
 ddsol.basis
 ddsol.prob.p  #perfect reconstruction if we set the ridge regression penalty to 0
 
-ddsol.prob
-
-
-sol = solve(sol.prob, Tsit5())
-
-plot(ode_sol, idxs=[1], tspan=(0, 5), label="lorenz", title="on training data")
-plot!(sol, idxs=[1], tspan=(0, 5), label="SINDy")
-
-plot(ode_sol, idxs=[1], tspan=(50, 100), label="lorenz", title="on validation data")
-plot!(sol, idxs=[1], tspan=(50, 100), label="SINDy")
