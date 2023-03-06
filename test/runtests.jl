86dda1564056bd1b025ae7b75d7af404b4a45b7b
tobiasgerwald <71821271+TobiasGerwald@users.noreply.github.com>
2023-03-06 14:16:48 +0100

    add test for Lorenz data generation

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,6 @@ using Test
 
 @testset "MMLDS_project.jl" begin
     # Write your tests here.
+    # These tests are executed every time we call "]test MMLDS_project".
+    include("data_tests.jl")
 end
